7c90bbd13eff1538502df56d3d63e2bd2b5a7f93
Paolo <paolo@growthlabs.ca>
2014-12-01 06:20:18 -0500

    Took out admin bar

--- wp-content/themes/marketify/functions.php
+++ wp-content/themes/marketify/functions.php
@@ -916,3 +916,18 @@ function getSizeFile($url) {
 
     return number_format($x/1024/1024,2);
 } 
+
+//Get rid of admin bar
+add_action('after_setup_theme', 'remove_admin_bar');
+
+ 
+
+function remove_admin_bar() {
+
+if (!current_user_can('administrator') && !is_admin()) {
+
+  show_admin_bar(false);
+
+}
+
+}
